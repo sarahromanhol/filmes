e9e2ac69601289a81b081374496fa3ec497e0076
Sarah Romanhol Falconiere <sarahromanhol@gmail.com>
2022-01-12 16:48:20 -0300

    alteracies

--- src/endpoints/createCharacter.ts
+++ src/endpoints/createCharacter.ts
@@ -1,17 +1,22 @@
-// import { Request, Response } from "express";
-// import { characters } from "../data";
-
-// export default function createCharacter(req: Request, res: Response): void {
-
-//     const { name, gender, description } = req.body
-
-//     characters.push({
-//         id: Date.now(),
-//         name,
-//         gender,
-//         description
-//     })
+import { Request, Response } from "express";
+import connection from "../connection";
 
 
-//     res.status(201).end()
-// }
+export default async function createCharacter(req: Request, res: Response): Promise<void> {
+
+    try {
+        const { name, gender, description } = req.body
+
+        await connection("character")
+            .insert({ name, gender, description })
+
+
+        res.status(201).end()
+
+    } catch (error) {
+        res.status(500).end()
+
+    }
+
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import app from './app'
+import createCharacter from './endpoints/createCharacter'
 import deleteCharacter from './endpoints/deleteCharacter'
 import getAllCharacters from './endpoints/getAllCharacters'
-// import createCharacter from './endpoints/createCharacter'
+
 
 
 app.get("/character", getAllCharacters)
-// app.put("/character", createCharacter)
+app.put("/character", createCharacter)
 app.delete("/character/:id", deleteCharacter)

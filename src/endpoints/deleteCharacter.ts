bf0021fa37c30cb34e5bb837b82c49a98f3ccb52
Sarah Romanhol Falconiere <sarahromanhol@gmail.com>
2022-01-12 14:37:02 -0300

    criação de enpoints

---
+++ src/endpoints/deleteCharacter.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { characters } from "../data";
+
+export default function deleteCharacter(req: Request, res: Response): void {
+
+    const { id } = req.params
+
+    const index: number = characters.findIndex(
+        character => character.id === Number(id)
+    )
+
+
+    if (index > -1) characters.splice(index, 1)
+
+    res.status(200).end()
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import app from './app'
 import createCharacter from './endpoints/createCharacter'
+import deleteCharacter from './endpoints/deleteCharacter'
 import getAllCharacters from './endpoints/getAllCharacters'
 
 app.get("/character", getAllCharacters)

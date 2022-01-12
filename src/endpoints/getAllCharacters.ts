af2948f3b9f3023311cb373c110baf30019c4fb6
Sarah Romanhol Falconiere <sarahromanhol@gmail.com>
2022-01-12 17:03:30 -0300

    alteracoes finais

--- src/endpoints/getAllCharacters.ts
+++ src/endpoints/getAllCharacters.ts
@@ -5,9 +5,17 @@ import { character } from "../types";
 export default async function getAllCharacters(req: Request, res: Response): Promise<void> {
 
     try {
-        const name = req.query
+        const {name, orderBy, orderType, page } = req.query
+
+        const resultsPerPage = 5
+
+        const offset = resultsPerPage * (Number(page) - 1)
 
         const characters: character[] = await connection("character")
+            .where("name", "LIKE", `%${name}%`)
+            .orderBy(orderBy as string || "name", orderType as string)
+            .offset(offset)
+
 
 
         res.send(characters)
